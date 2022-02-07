8f60106e3a1f28b65bb05067c5d9161c9a638a7b
schlopp <god.tcs@gmail.com>
2022-02-07 18:07:03 +0100

    More helper functions

--- cogs/impersonation.py
+++ cogs/impersonation.py
@@ -1,6 +1,6 @@
 import asyncio
 import random
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, Union
 
 import discord  # type: ignore
 from discord.ext import commands, vbu  # type: ignore
@@ -41,12 +41,43 @@ class ImpersonationCommands(vbu.Cog):
             ],
         ]
 
+    async def get_webhook(self, channel: discord.TextChannel) -> discord.Webhook:
+        if channel.id in self._webhook_cache:
+            webhook = self._webhook_cache[channel.id]
+        else:
+            for i in await channel.webhooks():
+                if i.name == "impersonator":
+                    webhook = i
+                    break
+            else:
+                avatar = await self.bot.user.avatar.read()
+                webhook = await channel.create_webhook(
+                    name="impersonator",
+                    avatar=avatar,
+                    reason="This is used by the impersonator bot.",
+                )
+            self._webhook_cache[channel.id] = webhook
+        return webhook
+
+    async def send_user_webhook(
+        self,
+        webhook: discord.Webhook,
+        user: Union[discord.User, discord.Member],
+        message: str,
+    ):
+        await webhook.send(
+            content=message,
+            username=user.display_name,
+            avatar_url=user.avatar.url if user.avatar else None,
+            allowed_mentions=discord.AllowedMentions.none(),
+        )
+
     @commands.command(name="say")
+    @commands.guild_only()
+    @commands.is_slash_command()
     @commands.bot_has_permissions(
         send_messages=True, embed_links=True, manage_webhooks=True
     )
-    @commands.guild_only()
-    @commands.is_slash_command()
     async def _say_command(
         self, ctx: commands.SlashContext, user: discord.Member, *, message: str
     ):
@@ -54,44 +85,18 @@ class ImpersonationCommands(vbu.Cog):
         Make a user say anything you want!
         """
 
-        if not isinstance(ctx.channel, discord.TextChannel):
-            return await ctx.interaction.response.send_message(
-                "This command can only be used in text channels."
-            )
-
-        if ctx.channel.id in self._webhook_cache:
-            webhook = self._webhook_cache[ctx.channel.id]
-        else:
-            for i in await ctx.channel.webhooks():
-                if i.name == "impersonator":
-                    webhook = i
-                    break
-            else:
-                avatar = await ctx.guild.me.avatar.read()
-                webhook = await ctx.channel.create_webhook(
-                    name="impersonator",
-                    avatar=avatar,
-                    reason="This is used by the impersonator bot.",
-                )
-            self._webhook_cache[ctx.channel.id] = webhook
-
-        await webhook.send(
-            content=message,
-            username=user.display_name,
-            avatar_url=user.avatar.url if user.avatar else None,
-            allowed_mentions=discord.AllowedMentions(
-                everyone=False, users=False, roles=False, replied_user=False
-            ),
-        )
+        webhook = await self.get_webhook(ctx.channel)
+        await self.send_user_webhook(webhook, user, message)
 
         # Zero-width space, to make the emoji appear smaller
         await ctx.interaction.response.send_message(f":eyes:{ZERO_WIDTH_SPACE}")
 
     @commands.command(name="convo")
+    @commands.guild_only()
+    @commands.is_slash_command()
     @commands.bot_has_permissions(
         send_messages=True, embed_links=True, manage_webhooks=True
     )
-    @commands.guild_only()
     async def _convo_command(
         self,
         ctx: commands.SlashContext,
@@ -102,37 +107,15 @@ class ImpersonationCommands(vbu.Cog):
         Generate a fake convorsation between two users!
         """
 
-        if not isinstance(ctx.channel, discord.TextChannel):
-            return await ctx.interaction.response.send_message(
-                "This command can only be used in text channels."
-            )
-
         await ctx.interaction.response.defer()
 
-        if ctx.channel.id in self._webhook_cache:
-            webhook = self._webhook_cache[ctx.channel.id]
-        else:
-            for webhook in await ctx.channel.webhooks():
-                if webhook.name == "impersonator":
-                    webhook = webhook
-                    break
-            else:
-                avatar = await ctx.guild.me.avatar.read()
-                webhook = await ctx.channel.create_webhook(
-                    name="impersonator",
-                    avatar=avatar,
-                    reason="This is used by the impersonator bot.",
-                )
-            self._webhook_cache[ctx.channel.id] = webhook
-
         convo = random.choice(self._convos)
+        webhook = await self.get_webhook(ctx.channel)
+
         for convo_user_id, convo_message in convo:
             convo_user = first_user if not convo_user_id else second_user
-            await webhook.send(
-                content=convo_message.format(second_user.display_name),
-                username=convo_user.display_name,
-                avatar_url=convo_user.avatar.url if convo_user.avatar else None,
-                allowed_mentions=discord.AllowedMentions.none(),
+            await self.send_user_webhook(
+                webhook, convo_user, convo_message.format(convo_user.display_name)
             )
             await asyncio.sleep(1)
 
